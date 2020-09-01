69824406006b8560f3a76857e5a1ecd1fd4faacd
陈家伟 <2498692241@qq.com>
2020-09-01 20:00:55 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputVal" @change="handleInputChanged" />
+    <a-button type="primary" @click="addItem">添加事项</a-button>
+
+    <a-list bordered :dataSource="infoList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox :checked="item.done" @change="(e)=>{cbChanged(e,item.id)}">{{item.info}}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="deleteItem(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{unDone}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="viewKey === '1' ? 'primary':'default'" @click="changeList('1')">全部</a-button>
+          <a-button :type="viewKey === '2' ? 'primary':'default'" @click="changeList('2')">未完成</a-button>
+          <a-button :type="viewKey === '3' ? 'primary':'default'" @click="changeList('3')">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="deleteAll">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mapState, mapGetters } from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    this.$store.dispatch('getList')
+  },
+  computed: {
+    ...mapState(['list', 'inputVal', 'viewKey']),
+    ...mapGetters(['unDone', 'infoList'])
+  },
+  methods: {
+    handleInputChanged (e) {
+      this.$store.commit('setInputVal', e.target.value)
+    },
+    addItem () {
+      if (this.inputVal.trim().lenght <= 0) {
+        return this.$message.warning('请输入!')
+      }
+      this.$store.commit('addItem')
+    },
+    deleteItem (id) {
+      this.$store.commit('deleteItem', id)
+    },
+    cbChanged (e, id) {
+      const params = {
+        id: id,
+        checked: e.target.checked
+      }
+      this.$store.commit('c', params)
+    },
+    deleteAll () {
+      this.$store.commit('deleteAll')
+    },
+    changeList (key) {
+      this.$store.commit('changeList', key)
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+  margin: 0 auto;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,74 @@
 import Vue from 'vue'
+
 import Vuex from 'vuex'
+
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [],
+    inputVal: '',
+    nextId: 5,
+    viewKey: '1'
   },
   mutations: {
+    saveList (state, step) {
+      state.list = step
+    },
+    setInputVal (state, val) {
+      state.inputVal = val
+    },
+    addItem (state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputVal,
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputVal = ''
+    },
+    deleteItem (state, id) {
+      const i = state.list.findIndex(item => item.id === id)
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    c (state, params) {
+      const i = state.list.findIndex(item => item.id === params.id)
+      state.list[i].done = params.checked
+    },
+    deleteAll (state) {
+      state.list = state.list.filter(i => i.done === false)
+    },
+    changeList (state, key) {
+      this.state.viewKey = key
+    }
   },
   actions: {
+    getList (context) {
+      axios.get('/1.json').then(({ data }) => {
+        context.commit('saveList', data)
+      })
+    }
   },
-  modules: {
+  getters: {
+    unDone (state) {
+      return state.list.filter(i => i.done === false).length
+    },
+    infoList (state) {
+      if (state.viewKey === '1') {
+        return state.list
+      }
+      if (state.viewKey === '2') {
+        return state.list.filter(x => !x.done)
+      }
+      if (state.viewKey === '3') {
+        return state.list.filter(x => x.done)
+      }
+      return state.list
+    }
   }
 })
